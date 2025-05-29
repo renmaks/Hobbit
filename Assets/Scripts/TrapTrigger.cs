6f9b371be57be2ba8ef0f554bdd2d053eb8771f0
renmaks <renmaks@duck.com>
2025-05-29 16:36:10 +0500

    Всё-таки триггер работает криво, разбираюсь

--- Assets/Scripts/TrapTrigger.cs
+++ Assets/Scripts/TrapTrigger.cs
@@ -5,6 +5,7 @@ public class TrapTrigger : MonoBehaviour
 	private void OnCollisionEnter(Collision collision)
 	{
 		var damageable = collision.gameObject.GetComponent<IDamageable>();
+		Debug.Log("Collision with " + collision.gameObject.name);
 		damageable?.Die();
 	}
 }
